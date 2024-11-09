4be9f23ffca8471c387aeed398fc809f601ebc6d
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-09 19:52:08 +0530

    Card wrapper integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-// Write your code at relevant places in the code below
-
-
 import Expenses from "./components/Expenses";
 
 function App() {
-
+  const expenses = [
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
+  ];
 
   return (
     <div>
-      <h1>Let's get Started</h1>
-      <Expenses></Expenses>
-      
+      <h2>Let's get Started</h2>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,28 +1,24 @@
 // Write your code here
+
 import ExpenseItem from "./ExpenseItem";
-const expenses = [
-  { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
-  { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
-  { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
-  { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
-];
+import "./Expenses.css";
+import Card from "./Card";
 
-function Expenses(){
-
-
-      return expenses.map((expense, index) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            price={expense.price}
-          ></ExpenseItem>
-        );
-      });
-
-
+function Expenses(props) {
+    return (
+        <Card className='expenses'>{
+            props.expenses.map((expense)=>{
+                return (
+                    <ExpenseItem
+                        id={expense.id}
+                        title={expense.title}
+                        date={expense.date}
+                        price={expense.price}
+                    />
+                );
+            })}
+        </Card>
+    );
 }
-
 
 export default Expenses;
