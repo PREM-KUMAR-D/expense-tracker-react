52b2c1649f5769e3c23027eb26178d8682552529
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-09 19:00:48 +0530

    Card Styling and separating to components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import ExpenseItem from "./components/ExpenseItem";
+// Write your code at relevant places in the code below
+
+
+import Expenses from "./components/Expenses";
 
 function App() {
-  const expenses = [
-    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
-    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
-    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
-    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
-  ];
+
 
   return (
     <div>
-      <h2>Let's get Started</h2>
-      {expenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            date={expense.date}
-            price={expense.price}
-          ></ExpenseItem>
-        );
-      })}
+      <h1>Let's get Started</h1>
+      <Expenses></Expenses>
+      
     </div>
   );
 }
