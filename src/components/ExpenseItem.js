90436485eee6441f0fda14af43231a4b8a63c229
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-09 18:50:11 +0530

    added dates to the expense list

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,29 @@
-// Write your code at relevant places in the code below:
+// Write your code here
+// Write your code here
 
 import "./ExpenseItem.css";
 
+const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 function ExpenseItem(props) {
+  const month = months[props.date.getMonth()];
+  const year = props.date.getFullYear();
+  const date = props.date.getDate();
+
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__location'>
-        {props.location}
-      </div>
+    
+          <div>
+              <div>{month} </div>
+              <div>{year} </div>
+                <div>{date}</div>
+
+          </div>
+    
+      <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
