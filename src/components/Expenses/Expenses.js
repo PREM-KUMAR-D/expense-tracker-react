4999285e9100a0d48ba4649a9c65cd379c2f1875
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-11 10:46:54 +0530

    condtional rendering for one item

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,47 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
 function Expenses(props) {
-  const [filteredYear,setFilteredYear]= useState("2023");
+  const [filteredYear, setFilteredYear] = useState("2023");
 
-  const changeFilterHandler = (selectedYear)=>{
+  const changeFilterHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p> No Expenses Found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          date={expense.date}
+          price={expense.price}
+        />
+      );
+    });
   }
+  let oneContent = <p> Only one expense here. Please add more </p>;
 
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter ={changeFilterHandler} />
-      {props.expenses
-      .filter((expense)=>{
-        return expense.date.getFullYear().toString() === filteredYear;
-      })
-      .map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            date={expense.date}
-            price={expense.price}
-          />
-        );
-      })}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {expensesContent}
+      {
+        filteredExpenses.length ===1 && oneContent
+      }
     </Card>
   );
 }
